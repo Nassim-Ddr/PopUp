f40b7710b2e5e0690fa6e3d9bfdb2b38e8650910
Nassim-Ddr <aa.nassim.ddr@gmail.com>
2019-09-15 11:29:26 +0100

    Started PortalShoot script

---
+++ PopUp/Assets/Player/PortalBullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalBullet : MonoBehaviour
+{
+    public bool In;
+    public float Velocity;
+    public GameObject PortalIn;
+    public GameObject PortalOut;
+
+    private Rigidbody2D Rb;
+
+    void Start()
+    {
+        Rb = GetComponent<Rigidbody2D>();
+        Rb.velocity = transform.right * Velocity;
+    }
+
+    void OnTriggerEnter2D(Collider2D HitInfo)
+    {
+        if (HitInfo.tag != "Player")
+        {
+            if (In)
+            Instantiate(PortalIn, transform.position, transform.rotation);
+            else Instantiate(PortalOut, transform.position, transform.rotation);
+            Destroy(gameObject, 0);
+        }
+    }
+}
--- PopUp/Assets/Player/PortalShoot.cs
+++ PopUp/Assets/Player/PortalShoot.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 public class PortalShoot : MonoBehaviour
 {
     public Transform ShootPoint;
+    public GameObject PortalInBullet;
+    public GameObject PortalOutBullet;
+
 
     void Update()
     {
-        if(Input.GetButtonDown("Shoot"))
-        {
-            Shoot();
-        }
+        if(Input.GetButtonDown("PortalInShoot")) PortalInShoot();
+        else if (Input.GetButtonDown("PortalOutShoot")) PortalOutShoot(); //Used else to prevent shooting both portals at the same time
     }
 
-    void Shoot()
+    void PortalInShoot()
     {
+        Instantiate(PortalInBullet, ShootPoint.position, ShootPoint.rotation);
+    }
 
+    void PortalOutShoot() 
+    {
+        Instantiate(PortalOutBullet, ShootPoint.position, ShootPoint.rotation);
     }
 }
