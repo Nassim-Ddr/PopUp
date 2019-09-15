27723a35f61744bb3f541bc28e865f24eeb734da
Nassim-Ddr <aa.nassim.ddr@gmail.com>
2019-09-15 17:59:03 +0100

    Finished the shooting portals scripts .

---
+++ PopUp/Assets/Player/Scripts/PortalShoot.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalShoot : MonoBehaviour
+{
+    public Transform ShootPoint;
+    public GameObject PortalInBullet;
+    public GameObject PortalOutBullet;
+    public PortalController PC;
+
+    private Transform Parent;
+    
+
+    void Start()
+    {
+        Parent = GameObject.Find("PortalController").transform;
+    }
+
+    void Update()
+    {
+        Vector3 Diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float RotZ = Mathf.Atan2(Diff.y, Diff.x) * Mathf.Rad2Deg;
+        ShootPoint.rotation = Quaternion.Euler(0f, 0f, RotZ);
+
+
+        if(Input.GetButtonDown("PortalInShoot")) PortalInShoot();
+        else if (Input.GetButtonDown("PortalOutShoot")) PortalOutShoot(); //Used else to prevent shooting both portals at the same time
+    }
+
+    void PortalInShoot()
+    {
+        //BULLETONEFFECT
+        PC.PortalCheck(true);
+        Instantiate(PortalInBullet, ShootPoint.position, ShootPoint.rotation,Parent);
+    }
+
+    void PortalOutShoot() 
+    {
+        //BULLETONEFFECT
+        PC.PortalCheck(false);
+        Instantiate(PortalOutBullet, ShootPoint.position, ShootPoint.rotation,Parent);
+    }
+}
--- PopUp/Assets/Portals/Scripts/PortalBullet.cs
+++ PopUp/Assets/Portals/Scripts/PortalBullet.cs
@@ -10,6 +10,7 @@ public class PortalBullet : MonoBehaviour
     public GameObject PortalOut;
 
     private Rigidbody2D Rb;
+    private Quaternion Rotation;
 
     void Start()
     {
@@ -19,12 +20,14 @@ public class PortalBullet : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D HitInfo)
     {
+        Rotation = Quaternion.Euler(0f, 0f, HitInfo.transform.rotation.eulerAngles.z - 90); //Adjust portal rotation depending on the support that the bullet is landing on
         if (HitInfo.tag != "Player")
         {
             if (In)
-            Instantiate(PortalIn, transform.position, transform.rotation);
-            else Instantiate(PortalOut, transform.position, transform.rotation);
+            Instantiate(PortalIn, transform.position, Rotation, gameObject.transform.parent.transform);
+            else Instantiate(PortalOut, transform.position, Rotation, gameObject.transform.parent.transform);
             Destroy(gameObject, 0);
+            //BULLETOFFEFFECT OR PORTALONEFFECT
         }
     }
 }
