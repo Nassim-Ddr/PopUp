2f3213424881607d6ab8a915c952017addcfe7ce
Nassim-Ddr <aa.nassim.ddr@gmail.com>
2019-09-16 13:46:50 +0100

    Finished Portals.cs (to travel from a portal to an other)

--- PopUp/Assets/Portals/Scripts/PortalBullet.cs
+++ PopUp/Assets/Portals/Scripts/PortalBullet.cs
@@ -21,7 +21,7 @@ public class PortalBullet : MonoBehaviour
     void OnTriggerEnter2D(Collider2D HitInfo)
     {
         Rotation = Quaternion.Euler(0f, 0f, HitInfo.transform.rotation.eulerAngles.z - 90); //Adjust portal rotation depending on the support that the bullet is landing on
-        if (HitInfo.tag != "Player")
+        if (HitInfo.tag != "Player" && HitInfo.tag != "Portal")
         {
             if (In)
             Instantiate(PortalIn, transform.position, Rotation, gameObject.transform.parent.transform);
--- PopUp/Assets/Portals/Scripts/Portals.cs
+++ PopUp/Assets/Portals/Scripts/Portals.cs
@@ -4,8 +4,20 @@ using UnityEngine;
 
 public class Portals : MonoBehaviour
 {
+    public bool In;
+    private Transform Destination;
+
     void OnTriggerEnter2D(Collider2D HitInfo)
     {
-        
+        if (Vector3.Distance(HitInfo.transform.position, transform.position) > 0.3f)
+        {
+            if (In) Destination = GameObject.Find("PortalOut(Clone)").GetComponent<Transform>();
+            else Destination = GameObject.Find("PortalIn(Clone)").GetComponent<Transform>();
+
+            if (Destination != null)
+            {
+                HitInfo.transform.position = Destination.position;
+            }
+        }
     }
 }
