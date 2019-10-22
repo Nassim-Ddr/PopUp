ba0aee67651a3f450f8630ab38386561dffe4efa
Nassim-Ddr <aa.nassim.ddr@gmail.com>
2019-10-22 13:29:31 +0100

    Camera Swap Script

---
+++ PopUp/Assets/Environment/Scripts/CameraManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public Transform[] Views;
+    public Camera Cam;
+
+
+    private int i = 0;
+    
+
+    void OnTriggerExit2D(Collider2D HitInfo)
+    {
+        if (HitInfo.name == "Player")
+        {
+            i = (i + 1) % 2;
+            Cam.transform.position = Views[i].position;
+        }
+    }
+}
